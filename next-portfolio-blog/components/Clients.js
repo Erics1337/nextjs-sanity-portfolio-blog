87338bbb1b1fe6fa9e2f8428901f4ff394ebb25b
Eric <erics1337@gmail.com>
2021-10-31 16:29:39 -0600

    Clients

--- next-portfolio-blog/components/Clients.js
+++ next-portfolio-blog/components/Clients.js
@@ -1,49 +1,38 @@
+import Image from 'next/Image'
+
 function Clients() {
-    return (
-        <section>
-            <div className="bg-grey-50"
-     id="clients">
-    <div className="container py-16 md:py-20">
-        <div className="w-full sm:w-3/4 lg:w-full mx-auto">
-            <h2
-                className="font-header font-semibold text-primary text-4xl sm:text-5xl lg:text-6xl uppercase text-center">
-                My latest clients</h2>
-            <div
-                 className="flex flex-wrap items-center justify-center pt-4 sm:pt-4">
-                 <span className="block m-8">
-                    <img src="/img/logo-coca-cola.svg"
-                         alt="client logo"
-                         className="mx-auto block h-12 w-auto" />
-                 </span>
-                 <span className="block m-8">
-                    <img src="/img/logo-apple.svg"
-                         alt="client logo"
-                         className="mx-auto block h-12 w-auto" />
-                 </span>
+  return (
+    <section>
+      <div className="bg-grey-50" id="clients">
+        <div className="container py-16 md:py-20">
+          <div className="w-full sm:w-3/4 lg:w-full mx-auto">
+            <h2 className="font-header font-semibold text-primary text-4xl sm:text-5xl lg:text-6xl uppercase text-center">
+              My latest clients
+            </h2>
+            <div className="flex flex-wrap items-center justify-center pt-4 sm:pt-4">
+              <span className="block m-8">
+              <Image
+                src="/beiLogo.png"
+                className="mx-auto block h-12 w-auto"
+                alt="Client Logo"
+                height={80} width={100}
+                />
+              </span>
+              <span className="block m-8">
+              <Image
+                src="/tinyFeet.png"
+                className="mx-auto block h-12 w-auto"
+                alt="Client Logo"
+                height={80} width={250}
+                />
+              </span>
 
-                 <span className="block m-8">
-                    <img src="/img/logo-netflix.svg"
-                         alt="client logo"
-                         className="mx-auto block h-12 w-auto" />
-                 </span>
-
-                 <span className="block m-8">
-                    <img src="/img/logo-amazon.svg"
-                         alt="client logo"
-                         className="mx-auto block h-12 w-auto" />
-                 </span>
-
-                 <span className="block m-8">
-                    <img src="/img/logo-stripe.svg"
-                         alt="client logo"
-                         className="mx-auto block h-12 w-auto" />
-                 </span>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-        </section>
-    )
+      </div>
+    </section>
+  )
 }
 
 export default Clients
