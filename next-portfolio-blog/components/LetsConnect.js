98a4c070d57858cd9b34bad18a723389131ea213
Eric <erics1337@gmail.com>
2022-06-10 10:33:32 -0600

    btn navBtn popup effect class

--- next-portfolio-blog/components/LetsConnect.js
+++ next-portfolio-blog/components/LetsConnect.js
@@ -1,34 +1,57 @@
-import { ChevronRightIcon } from "@heroicons/react/solid";
-import Link from 'next/link';
-import Image from 'next/image';
-
+import { ChevronRightIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import Image from 'next/image'
 
 function LetsConnect() {
-    return (
-        <div className="flex flex-col sm:flex-row justify-center lg:justify-start pt-5 sm:pt-5">
-        <div className="flex justify-center sm:justify-start items-center pl-0 md:pl-1">
-          <p className="font-body text-gray-800 text-lg uppercase pr-0">
-            Let's connect
-          </p>
+	return (
+		<div className='flex flex-col sm:flex-row justify-center lg:justify-start pt-5 sm:pt-5'>
+			<div className='flex justify-center sm:justify-start items-center pl-0 md:pl-1'>
+				<p className='font-body text-gray-800 text-lg uppercase pr-0'>
+					Let's connect
+				</p>
 
-        {/* Icons */}
-        <div className="flex items-center justify-center sm:justify-start sm:pt-0">
-        <div className="pr-2">
-            <ChevronRightIcon height={30} width={30}/>
-        </div>
-        <div className="px-2">
-            <Link href='https://github.com/Erics1337'><Image className="cursor-pointer" src="/img/gitHub.png" alt="GitHub" height={30} width={30} /></Link>
-          </div>
-          <div className="px-2">
-            <Link href='https://www.linkedin.com/in/ericswansondesign/'><Image className="cursor-pointer" src="/img/linkedin.png" alt="GitHub" height={30} width={30} /></Link>
-          </div>
-          <div className="px-2">
-            <Link href='https://twitter.com/don_swan'><Image className="cursor-pointer" src="/img/twitter.png" alt="GitHub" height={40} width={40} /></Link>
-          </div>
-        </div>
-        </div>
-      </div>
-    )
+				{/* Icons */}
+				<div className='flex items-center justify-center sm:justify-start sm:pt-0'>
+					<div className='pr-2'>
+						<ChevronRightIcon height={30} width={30} />
+					</div>
+					<div className='px-2 btn navBtn'>
+						<Link href='https://github.com/Erics1337'>
+							<Image
+								className='cursor-pointer'
+								src='/img/gitHub.png'
+								alt='GitHub'
+								height={30}
+								width={30}
+							/>
+						</Link>
+					</div>
+					<div className='px-2 btn navBtn'>
+						<Link href='https://www.linkedin.com/in/ericswansondesign/'>
+							<Image
+								className='cursor-pointer'
+								src='/img/linkedin.png'
+								alt='GitHub'
+								height={30}
+								width={30}
+							/>
+						</Link>
+					</div>
+					<div className='px-2 btn navBtn'>
+						<Link href='https://twitter.com/don_swan'>
+							<Image
+								className='cursor-pointer'
+								src='/img/twitter.png'
+								alt='GitHub'
+								height={40}
+								width={40}
+							/>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default LetsConnect
--- next-portfolio-blog/components/Navbar.js
+++ next-portfolio-blog/components/Navbar.js
@@ -1,61 +1,128 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import { MenuIcon, HomeIcon } from "@heroicons/react/solid"
-
+import { MenuIcon, HomeIcon } from '@heroicons/react/solid'
 
 const Navbar = ({ open, setOpen }) => {
-    return (
-        <nav className="bg-gray-100">
-            <div className="max-w-6xl mx-auto px-4">
-                    <div className="flex justify-between px-8">
-                        <div className="flex space-x-4">
-                            <div className="mr-4">
-                                <a href="/" className="flex items-center py-5 px-3">                    
-                                    <HomeIcon className="h-5 w-5 mr-1 text-blue-500"/>
-                                <h1 className="font-semibold">Home</h1>
-                                </a>
-                            </div>
+	return (
+		<nav className='bg-gray-100'>
+			<div className='max-w-6xl mx-auto px-4'>
+				<div className='flex justify-between px-8 py-6'>
+					<div className='flex space-x-4'>
+						<div className='mr-4'>
+							<Link href='/'>
+								<div className='btn navBtn flex px-3'>
+									<HomeIcon className='h-5 w-5 mr-1 text-blue-500' />
+									<span className='font-semibold'>Home</span>
+								</div>
+							</Link>
+						</div>
 
-                            <div className="flex items-center space-x-5">
-                                <Link href="/portfolio" className="hidden sm:flex py-4 px-3 text-gray-700 hover:text-gray-900">Portfolio</Link>
-                                <Link href="/blog" className="hidden sm:flex py-4 px-3 text-gray-700 hover:text-gray-900">Blog</Link>
-                            </div>
-                            </div>
+						<div className='flex items-center space-x-5'>
+							<Link
+								href='/portfolio'
+								className='hidden sm:flex py-4 px-3 text-gray-700 hover:text-gray-900'>
+								<span className='btn navBtn'>Portfolio</span>
+							</Link>
+							<Link
+								href='/blog'
+								className='hidden sm:flex py-4 px-3 text-gray-700 hover:text-gray-900'>
+								<span className='btn navBtn'>Blog</span>
+							</Link>
+						</div>
+					</div>
 
-                            <div className="hidden lg:flex items-center space-x-5">
-                                <Link href="/#about" className="hidden lg:flex py-5 px-3">About</Link>
-                                <Link href="/#services" className="hidden lg:flex py-5 px-3">Services</Link>
-                                <Link href="/#projects" className="hidden lg:flex py-5 px-3">Projects</Link>
-                                <Link href="/#experience" className="hidden lg:flex py-5 px-3">Experience</Link>
-                                <Link href="/#stats" className="hidden lg:flex py-5 px-3">Stats</Link>
-                                <Link href="/#posts" className="hidden lg:flex py-5 px-3">Posts</Link>
-                                <Link href="/#contact" className="hidden md:flex py-5 px-3">Contact</Link>
-                            </div>
+					<div className='hidden lg:flex items-center space-x-5'>
+						<Link
+							href='/#about'
+							className='hidden lg:flex py-5 px-3'>
+							<span className='btn navBtn'>About</span>
+						</Link>
+						<Link
+							href='/#services'
+							className='hidden lg:flex py-5 px-3'>
+							<span className='btn navBtn'>Services</span>
+						</Link>
+						<Link
+							href='/#projects'
+							className='hidden lg:flex py-5 px-3'>
+							<span className='btn navBtn'>Projects</span>
+						</Link>
+						<Link
+							href='/#experience'
+							className='hidden lg:flex py-5 px-3'>
+							<span className='btn navBtn'>Experience</span>
+						</Link>
+						<Link
+							href='/#stats'
+							className='hidden lg:flex py-5 px-3'>
+							<span className='btn navBtn'>Stats</span>
+						</Link>
+						<Link
+							href='/#posts'
+							className='hidden lg:flex py-5 px-3'>
+							<span className='btn navBtn'>Posts</span>
+						</Link>
+						<Link
+							href='/#contact'
+							className='hidden md:flex py-5 px-3'>
+							<span className='btn navBtn'>Contact</span>
+						</Link>
+					</div>
 
-                            {/* Mobile button goes here */}
-                            <div className="lg:hidden flex items-center space-x-3">
-                                <button className="mobile-menu-button" open={open} onClick={() => setOpen(!open)}>
-                                    <MenuIcon className="h-6 w-6 mr-1 text-black" />
-                                </button>
-                            </div>
-                </div>
-            </div>
+					{/* Mobile button goes here */}
+					<div className='lg:hidden flex items-center space-x-3'>
+						<button
+							className='mobile-menu-button'
+							open={open}
+							onClick={() => setOpen(!open)}>
+							<MenuIcon className='h-6 w-6 mr-1 text-black' />
+						</button>
+					</div>
+				</div>
+			</div>
 
-            {/* Mobile menu */}
-            { open && 
-            <div >
-                <a href="/#whoAmI" className="block py-2 text-sm hover:bg-gray-200">Who Am I?</a>
-                <a href="/#services" className="block py-2 text-sm hover:bg-gray-200">Services</a>
-                <a href="/#portfolio" className="block py-2 text-sm hover:bg-gray-200">Portfolio</a>
-                <a href="/#experience" className="block py-2 text-sm hover:bg-gray-200">Experience</a>
-                <a href="/#stats" className="block py-2 text-sm hover:bg-gray-200">Stats</a>
-                <a href="/#posts" className="block py-2 text-sm hover:bg-gray-200">Posts</a>
-                <a  href="/#contact" className="block py-2 text-sm hover:bg-gray-200">Contact</a>
-            </div>
-            }
-        </nav>
-        
-    )
+			{/* Mobile menu */}
+			{open && (
+				<div>
+					<a
+						href='/#whoAmI'
+						className='block py-2 text-sm hover:bg-gray-200'>
+						Who Am I?
+					</a>
+					<a
+						href='/#services'
+						className='block py-2 text-sm hover:bg-gray-200'>
+						Services
+					</a>
+					<a
+						href='/#portfolio'
+						className='block py-2 text-sm hover:bg-gray-200'>
+						Portfolio
+					</a>
+					<a
+						href='/#experience'
+						className='block py-2 text-sm hover:bg-gray-200'>
+						Experience
+					</a>
+					<a
+						href='/#stats'
+						className='block py-2 text-sm hover:bg-gray-200'>
+						Stats
+					</a>
+					<a
+						href='/#posts'
+						className='block py-2 text-sm hover:bg-gray-200'>
+						Posts
+					</a>
+					<a
+						href='/#contact'
+						className='block py-2 text-sm hover:bg-gray-200'>
+						Contact
+					</a>
+				</div>
+			)}
+		</nav>
+	)
 }
 
 export default Navbar
