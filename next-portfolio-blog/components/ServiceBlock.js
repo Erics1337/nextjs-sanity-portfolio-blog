440b0c152ace5bb5aedc212448ce403b47235f10
Eric <erics1337@gmail.com>
2022-06-17 22:12:03 -0600

    Dark Mode Services Icons

--- next-portfolio-blog/components/ServiceBlock.js
+++ next-portfolio-blog/components/ServiceBlock.js
@@ -1,12 +1,44 @@
+import Image from 'next/image'
+
 function ServiceBlock({ image, hoverImage, title, body, link }) {
 	return (
 		<div className='shadow px-8 py-12 hover:bg-primary group rounded bg-white dark:bg-main-dark-bg'>
 			<div className='text-center w-24 xl:w-28 h-24 xl:h-28 mx-auto'>
-				<div className='hidden group-hover:block'>
-					<img src={image} alt='development icon' />
+				<div className='dark:hidden'>
+					<div className='hidden group-hover:block h-full w-full'>
+						<Image
+							src={image}
+							alt='development icon'
+							height={100}
+							width={100}
+						/>
+					</div>
+					<div className='block group-hover:hidden'>
+						<Image
+							src={hoverImage}
+							alt='development icon'
+							height={100}
+							width={100}
+						/>
+					</div>
 				</div>
-				<div className='block group-hover:hidden'>
-					<img src={hoverImage} alt='development icon' />
+				<div className='dark:block hidden'>
+					<div className='hidden group-hover:block h-full w-full'>
+						<Image
+							src={hoverImage}
+							alt='development icon'
+							height={100}
+							width={100}
+						/>
+					</div>
+					<div className='block group-hover:hidden'>
+						<Image
+							src={image}
+							alt='development icon'
+							height={100}
+							width={100}
+						/>
+					</div>
 				</div>
 			</div>
 			<div className='text-center'>
