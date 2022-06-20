70e6d4a62d63548c3d83190594b889a21d54e0ce
Eric <erics1337@gmail.com>
2022-06-19 19:45:02 -0600

    Add Google Analytics

--- next-portfolio-blog/pages/_app.js
+++ next-portfolio-blog/pages/_app.js
@@ -1,22 +1,40 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
-import React from 'react';
+import React from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
-
+	RecoilRoot,
+	atom,
+	selector,
+	useRecoilState,
+	useRecoilValue,
+} from 'recoil'
+import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }) {
-  return(
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  )
-    
+	// Google Analytics
+	const router = useRouter()
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			ga.pageview(url)
+		}
+		//When the component is mounted, subscribe to router changes
+		//and log those page views
+		router.events.on('routeChangeComplete', handleRouteChange)
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
+
+	return (
+		<RecoilRoot>
+			<Component {...pageProps} />
+		</RecoilRoot>
+	)
 }
 
 export default MyApp
