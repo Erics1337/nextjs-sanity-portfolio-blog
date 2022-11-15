53d55a85692ab34b2f904f24972246f2ec36d2a1
Eric <erics1337@gmail.com>
2022-11-15 16:08:43 -0500

    Reverse Order Timeline

--- next-portfolio-blog/components/Experience.js
+++ next-portfolio-blog/components/Experience.js
@@ -18,33 +18,6 @@ function Experience() {
 
 				<div className='w-full lg:w-2/3 mx-auto flex flex-col mt-12 relative'>
 					<span className='absolute inset-y-0 left-2/5 ml-10 w-0.5 bg-gray-40 dark:bg-gray-10 hidden md:block'></span>
-
-					<Timeline
-						image='/purchaseCollege.png'
-						dates='2010 - 2014'
-						title='SUNY Purchase College'
-						description='Bachelor of Arts (BA), Liberal Studies w/ focus in Media and Communications and a Minor in Media, Society, and the Arts'
-					/>
-
-					<Timeline
-						image='/western.png'
-						dates='2017 - 2020'
-						title='Western Colorado University'
-						description='Bachelor of Science (BS), Computer Science'
-					/>
-					<Timeline
-						image='/tinyFeet.png'
-						dates='2020 - 2021'
-						title='Tiny Feet Toolkit'
-						description='Lead engineer on a team with a Masters of Environmental Management student and a group of engineers from the Colorado School of Mines where we developed a web application that helped communities build a climate action plan.'
-					/>
-					<Timeline
-						image='/beiLogo.png'
-						dates='2021 - 2022'
-						title='Business Enterprise Institute'
-						description='Full Stack Developer. Designed and developed the homepage of the company website.
-                Collaborated with the client success and marketing teams to build custom tools to aid in client success. Monitored security, tested performance, and oversaw the development of new features in various internal applications.'
-					/>
 					<Timeline
 						image='/ktz_logo_square.webp'
 						dates='2022'
@@ -54,6 +27,31 @@ function Experience() {
 						various internal processes that allow admin to interface with the data model behind the proprietary TMS software 
 						built on top of the Salesforce platform. Redesigned company marketing website from the legacy codebase and integrated it with a headless CMS. 
 						Improved various internal processes and made additions to data model.'
+					/>
+					<Timeline
+						image='/beiLogo.png'
+						dates='2021 - 2022'
+						title='Business Enterprise Institute'
+						description='Full Stack Developer. Designed and developed the homepage of the company website.
+                Collaborated with the client success and marketing teams to build custom tools to aid in client success. Monitored security, tested performance, and oversaw the development of new features in various internal applications.'
+					/>
+					<Timeline
+						image='/tinyFeet.png'
+						dates='2020 - 2021'
+						title='Tiny Feet Toolkit'
+						description='Lead engineer on a team with a Masters of Environmental Management student and a group of engineers from the Colorado School of Mines where we developed a web application that helped communities build a climate action plan.'
+					/>
+					<Timeline
+						image='/western.png'
+						dates='2017 - 2020'
+						title='Western Colorado University'
+						description='Bachelor of Science (BS), Computer Science'
+					/>
+					<Timeline
+						image='/purchaseCollege.png'
+						dates='2010 - 2014'
+						title='SUNY Purchase College'
+						description='Bachelor of Arts (BA), Liberal Studies w/ focus in Media and Communications and a Minor in Media, Society, and the Arts'
 					/>
 				</div>
 			</div>
