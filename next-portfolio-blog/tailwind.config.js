5e05049e659c419681e44cd505512bd3fa26771a
Eric <erics1337@gmail.com>
2024-03-17 22:35:43 -0600

    Tailwind Colors

--- next-portfolio-blog/tailwind.config.js
+++ next-portfolio-blog/tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
 				black: '#000000',
 				yellow: '#f9e71c',
 				lila: '#e6e5ec',
-				purple: '#8c7ae6',
+				purple: '#9e432a',
 				green: '#00b894',
 				blue: '#00cec9',
 				'gray-10': '#6c6b6d',
@@ -57,6 +57,7 @@ module.exports = {
 				'secondary-dark-bg': '#33373E',
 				'light-gray': '#F7F7F7',
 				'half-transparent': 'rgba(0, 0, 0, 0.5)',
+				'bg-purple': 'rgba(67, 42, 158, 0)'
 			},
 			spacing: {
 				13: '3.25rem',
