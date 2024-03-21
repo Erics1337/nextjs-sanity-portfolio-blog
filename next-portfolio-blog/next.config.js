ad1ad5829786c1c51e4f43c2abf14354c657634e
Eric <erics1337@gmail.com>
2024-03-20 23:14:14 -0600

    Add Sanity Image Domain

--- next-portfolio-blog/next.config.js
+++ next-portfolio-blog/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	images: {
-		domains: ['assets.acme.com', 'my.spline.design', 'spline.design', 'api.github.com'],
+		domains: ['cdn.sanity.io', 'assets.acme.com', 'my.spline.design', 'spline.design', 'api.github.com'],
 	},
 	reactStrictMode: true,
 	experimental: {
