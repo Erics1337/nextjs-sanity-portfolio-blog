55c4d7ad6043b20ef24ae276131358bcfa2a602f
Eric <erics1337@gmail.com>
2024-03-20 23:15:21 -0600

    Add "See More Projects"

--- next-portfolio-blog/components/Projects.js
+++ next-portfolio-blog/components/Projects.js
@@ -1,8 +1,8 @@
-import imageUrlBuilder from '@sanity/image-url'
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
-import Link from 'next/link'
-import ProjectCard from './cards/ProjectCard'
+import imageUrlBuilder from "@sanity/image-url"
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+import Link from "next/link"
+import ProjectCard from "./cards/ProjectCard"
 
 function Portfolio({ projects }) {
 	const router = useRouter()
@@ -14,8 +14,8 @@ function Portfolio({ projects }) {
 		if (projects.length) {
 			// ImageBuilder function
 			const imgBuilder = imageUrlBuilder({
-				projectId: 'ulqdo09f',
-				dataset: 'production',
+				projectId: "ulqdo09f",
+				dataset: "production",
 			})
 
 			setMappedProjects(
@@ -36,22 +36,27 @@ function Portfolio({ projects }) {
 	}, [projects])
 
 	return (
-		<section id='projects' className={'dark:bg-main-dark-bg'}>
-			<div className='container py-16 md:py-20'>
-				<Link href={'/portfolio'}>
-					<h2 className='font-header cursor-pointer hover:text-primaryHover font-semibold text-primary text-4xl sm:text-5xl lg:text-6xl uppercase text-center'>
-						Check out my Portfolio
-					</h2>
-				</Link>
-				<h3 className='font-header font-medium text-xl sm:text-2xl lg:text-3xl pt-6 text-center'>
+		<section id="projects" className={"dark:bg-main-dark-bg"}>
+			<div className="container py-16 md:py-20">
+				<h2 className="font-header font-semibold text-primary text-4xl sm:text-5xl lg:text-6xl uppercase text-center">
+					Check out my Portfolio
+				</h2>
+				<h3 className="font-header font-medium text-xl sm:text-2xl lg:text-3xl pt-6 text-center">
 					Here are some examples of what I have created
 				</h3>
 
-				<div className='w-full sm:w-3/4 lg:w-full mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-10 pt-12'>
+				<div className="w-full sm:w-3/4 lg:w-full mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-10 pt-12">
 					{/* Projects */}
 					{mappedProjects.map((project, index) => (
 						<ProjectCard project={project} key={index} />
 					))}
+				</div>
+				<div className="flex justify-center pt-12">
+					<Link href="/portfolio">
+						<a className="inline-block bg-primary text-white font-medium text-lg py-2 px-4 rounded-lg hover:bg-primaryHover transition-all duration-300 mx-auto text-center dark:bg-primary-dark dark:hover:bg-secondary-dark">
+							See more projects...
+						</a>
+					</Link>
 				</div>
 			</div>
 		</section>
